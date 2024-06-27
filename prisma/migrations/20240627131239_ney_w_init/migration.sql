05e5874aff04015a6a8e422aa3ca2a04a561621c
talipckl <talipckl@gmail.com>
2024-06-27 19:47:18 +0300

    ss

---
+++ prisma/migrations/20240627131239_ney_w_init/migration.sql
@@ -0,0 +1,21 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[bookmarks] ALTER COLUMN [created_at] DATETIME2 NULL;
+ALTER TABLE [dbo].[bookmarks] ALTER COLUMN [updated_at] DATETIME2 NULL;
+ALTER TABLE [dbo].[bookmarks] ADD CONSTRAINT [bookmarks_created_at_df] DEFAULT CURRENT_TIMESTAMP FOR [created_at];
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Global()
+@Global() // heryerden erişim
 @Module({
   providers: [PrismaService],
   exports:[PrismaService]
